29a464500056d1b0c80e4056139931cb77dacd53
sandydoo <sandermel94@gmail.com>
2017-06-14 16:53:40 +0300

    Fix strict mode typo in fastboot module export

--- public/fastboot-is-mobile.js
+++ public/fastboot-is-mobile.js
@@ -3,7 +3,7 @@
   define('ismobilejs', ['exports'], function(exports) {
     'use strict';
 
-    let isMobileClass = FastBoot.require('ismobilejs');
+    var isMobileClass = FastBoot.require('ismobilejs');
     // Change the context so that when isMobile internally sets the results of
     // the user agent tests to the current scope, it doesn't use the FastBoot
     // global scope, which is full of unnecessary stuff.
