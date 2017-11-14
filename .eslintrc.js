d99acdc4ac3f28e259f4e1c968748fbb7c0d7472
sandydoo <sandermel94@gmail.com>
2017-11-14 14:01:29 +0300

    Upgrade to new modules API
    * Run eslint new modules fix and new modules codemod.
    * Add `ember-cli-eslint` and and `eslint-plugin-ember`.
    * Remove `ember-cli-shims`.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,44 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],
   env: {
     browser: true
   },
   rules: {
+    // override rules' settings here
+
+    // Ember
+    // General
+    'ember/local-modules': 'off',
+    'ember/no-observers': 'off',
+    'ember/no-side-effects': 'error',
+    'ember/named-functions-in-promises': 'off',
+    'ember/use-ember-get-and-set': 'error',
+    'ember/use-brace-expansion': 'error',
+    'ember/new-module-imports': 'error',
+    'ember/no-old-shims': 'error',
+
+    // Organization
+    'ember/order-in-components': 'error',
+    'ember/order-in-controllers': 'error',
+    'ember/order-in-models': 'error',
+    'ember/order-in-routes': 'error',
+
+    // Controllers
+    'ember/alias-model-in-controller': 'off',
+
+    // Components
+    'ember/avoid-leaking-state-in-components': 'error',
+    'ember/closure-actions': 'error',
+    'ember/no-on-calls-in-components': 'error',
+
+    // Routing
+    'ember/routes-segments-snake-case': 'error',
+    'ember/no-capital-letters-in-routes': 'off'
   }
 };
+
--- addon/services/is-mobile.js
+++ addon/services/is-mobile.js
@@ -1,8 +1,6 @@
-import Service from 'ember-service';
-import get, { getProperties } from 'ember-metal/get';
-import set from 'ember-metal/set';
-import computed from 'ember-computed';
-import getOwner from 'ember-owner/get';
+import Service from '@ember/service';
+import { computed, get, getProperties, set } from '@ember/object';
+import { getOwner } from '@ember/application';
 import isMobile from 'ismobilejs';
 
 export default Service.extend({
@@ -17,7 +15,7 @@ export default Service.extend({
     let queries = [];
     if (get(this, 'fastboot.isFastBoot')) {
       const headers = get(this, 'fastboot.request.headers');
-      const userAgent = headers.get('user-agent');
+      const userAgent = get(headers, 'user-agent');
       if (userAgent) {
         queries = getProperties(
           isMobile(userAgent),
