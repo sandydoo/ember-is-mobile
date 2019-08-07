2b96e1bbc7161a599d8a6b430b2b9f9e568a9990
amerr <amerrnath.21@gmail.com>
2019-08-07 17:03:06 +0200

    Revert test cases

--- tests/unit/services/is-mobile-test.js
+++ tests/unit/services/is-mobile-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('service:is-mobile', 'Unit | Service | is mobile', {
   // Specify the other units that are required for this test.
-  needs: ['service:fastboot']
+  // needs: ['service:foo']
 });
 
 // Replace this with your real tests.
