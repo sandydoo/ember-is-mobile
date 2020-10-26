eaab047a8d6688d93ad133a37ef4a2d145b95d5e
sandydoo <hey@sandydoo.me>
2020-10-26 14:14:06 +0300

    Update blueprint

--- blueprints/ember-is-mobile/index.js
+++ blueprints/ember-is-mobile/index.js
@@ -1,14 +1,18 @@
 /* eslint-env node */
 
 module.exports = {
-  normalizeEntityName: function() {
+  name: 'ember-is-mobile',
+
+  description: "Add ismobilejs to the project's dependencies",
+
+  normalizeEntityName() {
     // this prevents an error when the entityName is not specified.
   },
 
   // Make sure to install ismobilejs on the parent app. This ensures it is
   // installed on the FastBoot App Server when it runs `npm install` in the
   // dist folder.
-  afterInstall: function() {
-    return this.addPackagesToProject('ismobilejs', '^1.1.1');
+  afterInstall() {
+    return this.addPackageToProject('ismobilejs', '^1.1.1');
   }
 };
